e973d881e74c8e4bb8f256e2b05cf9ac13e6f959
Your Name <you@example.com>
2024-01-31 22:12:07 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log('Lets write javascript');
+
+async function getSongs(){
+    let a = await fetch('http://127.0.0.1:5500/songs/')
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName('a') 
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+        
+    }
+    // console.log(songs);
+    return songs
+
+}
+
+
+async function main(){
+    let songs = await getSongs()
+    console.log(songs);
+
+    var audio = new Audio(songs[1]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(duration);
+      });
+
+}
+
+main()
+
+
+
+
+
