99d446374f5b5c631da0df600e0823d1fee8f864
Your Name <you@example.com>
2024-02-04 15:46:34 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 console.log('Lets write javascript');
 
-async function getSongs(){
+let currentSong = new Audio();
+
+async function getSongs() {
     let a = await fetch('http://127.0.0.1:5500/songs/')
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div")
     div.innerHTML = response;
-    let as = div.getElementsByTagName('a') 
+    let as = div.getElementsByTagName('a')
     let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
-        
+
     }
     // console.log(songs);
     return songs
 
 }
 
+playMusic = (track) => {
+    // var audio = new Audio("/songs/" + track);
+    currentSong.src = "/songs/" + track
+    currentSong.play();
 
-async function main(){
+    // audio.addEventListener("loadeddata", () => {
+    //     let duration = audio.duration;
+    //     console.log(duration);
+    // });
+}
+
+
+async function main() {
+
     // get the list of all songs
     let songs = await getSongs()
     console.log(songs);
@@ -29,8 +43,8 @@ async function main(){
     //show all songs in the playlist
     let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUl.innerHTML =  songUl.innerHTML + 
-        `<li> 
+        songUl.innerHTML = songUl.innerHTML +
+            `<li> 
         <img class="invert" src="music.svg" alt="">
         <div class="info">
             <div>${song.replaceAll("%20", " ")}</div>
@@ -40,17 +54,19 @@ async function main(){
             <img class = "invert" src="play.svg" alt="">
         </div>        
         </li>`;
-        
+
     }
 
-    // var audio = new Audio(songs[5]);
-    // audio.play();
 
-    audio.addEventListener("loadeddata", () => {
-        let duration = audio.duration;
-        console.log(duration);
-      });
 
+    //attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        let play = e.getElementsByTagName('img')[1];
+        play.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+        })
+    })
 }
 
 main()
